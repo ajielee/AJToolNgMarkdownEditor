cbb1a13276f6fc81c8f33dc91853ca2de7cf022e
Leo <ajie.lee@gmail.com>
2015-07-25 18:22:06 +0200

    Adds tab system to display

--- js/app.js
+++ js/app.js
@@ -25,11 +25,48 @@ var markdownEditor = angular.module('markdownEditor', ['hc.marked'])
 
 // main controller
 markdownEditor.controller('mainCtrl', ['$scope', 'marked', function($scope, marked) {
+
+    $scope.showEditor = true;
+    $scope.showViewer = false;
+
+    //$scope.showInTwoCol = false;
+
+    (function() {
+        // if window's width large than 992px or equal to 992px ('md' in Bootstrap)
+        if (992 >= window.innerWidth) {
+            $scope.showInTwoCol = false;
+        } else {
+            $scope.showInTwoCol = true;
+        }
+        $('textarea').focus();
+    })();
+
+
     // sets default value for textarea
     $scope.markdownSource = '';
-
     // updates $scope.markdownDisplay if $scope.markdownSource changes
     $scope.$watch('markdownSource', function() {
         $scope.markdownDisplay = marked($scope.markdownSource);
     });
+    $scope.editorOn = function() {
+        $scope.showEditor = true;
+        $scope.showViewer = false;
+    }
+    $scope.viewerOn = function() {
+        $scope.showEditor = false;
+        $scope.showViewer = true;
+    }
+
+    $(window).resize(function(){
+        // if window's width large than 992px or equal to 992px ('md' in Bootstrap)
+        if (992 >= window.innerWidth) {
+            $scope.showInTwoCol = false;
+            $scope.showEditor = true;
+            $scope.showViewer = false;
+        } else {
+            $scope.showInTwoCol = true;
+        }
+        $scope.$apply();
+    });
+
 }]);
